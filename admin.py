bcfb12a6bca8c30b15a097af5b4c468e1cf7c130
BishalBaniya <baniyabishal007@gmail.com>
2026-07-10 00:52:52 +0545

    Add Admin class with initialization

---
+++ admin.py
@@ -0,0 +1,9 @@
+class Admin:
+  def __init__(self, admin_id, username, password):
+    self.admin_id = admin_id
+    self.username = username
+    self.password = password
+
+
+a1 = Admin("1", "admin", "admin")
+# print(a1)
